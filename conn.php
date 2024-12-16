7d3d09c587a65af7bb1f5396e38df287c95a0408
RoxineAnneRegalado <146078207+RoxineAnneRegalado@users.noreply.github.com>
2024-12-15 23:37:52 -0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
   define('DB_SERVER', 'localhost');
   define('DB_USERNAME', 'adminroxine');
-  define('DB_PASSWORD', '@adminroxine123!');
+  define('DB_PASSWORD', '@admminroxine123!');
   define('DB_NAME', 'student');
 
   $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
